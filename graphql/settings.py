dd6b6b0b5b4b68a1a6cf5e0bf1fffc1ac59cfbdc
Shital Babu Luitel <ctalluitel@gmail.com>
2022-08-12 20:26:06 +0545

    Added file for package files for project

---
+++ graphql/settings.py
@@ -0,0 +1,108 @@
+from django.conf import settings
+from django.test.signals import setting_changed
+from django.utils.module_loading import import_string
+
+from graphql.defaults import get_defaults
+
+
+def perform_import(val, setting_name):
+    """
+    If the given setting is a string import notation,
+    then perform the necessary import or imports.
+    """
+    if val is None:
+        return None
+    elif isinstance(val, str):
+        return import_from_string(val, setting_name)
+    elif isinstance(val, (list, tuple)):
+        return [import_from_string(item, setting_name) for item in val]
+    return val
+
+
+def import_from_string(val, setting_name):
+    """
+    Attempt to import a class from a string representation.
+    """
+    try:
+        return import_string(val)
+    except ImportError as e:
+        msg = "Could not import '%s' for API setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
+        raise ImportError(msg)
+
+
+class ImportSettings:
+    def defaults(self):
+        used_payments = getattr(settings, 'USED_PAYMENTS')
+        params = get_defaults(used_payments)
+        new_defaults = {}
+        for key, value in params.items():
+            try:
+                new_defaults[key] = getattr(settings, key, value)
+            except Exception as e:  # noqa
+                new_defaults[key] = value
+
+        return new_defaults
+
+
+_settings = ImportSettings()
+DEFAULTS = _settings.defaults()
+
+
+class APISettings:
+    """
+    A settings object that allows REST Framework settings to be accessed as
+    properties. For example:
+        from common.settings import api_settings
+        print(api_settings.ACCEPTED_DOCUMENT_FORMATS)
+    Any setting with string import paths will be automatically resolved
+    and return the class, rather than the string literal.
+    Note:
+    This is an internal class that is only compatible with settings namespaced
+    under the COMMON name. It is not intended to be used by 3rd-party
+    apps, and test helpers like `override_settings` may not work as expected.
+    """
+
+    def __init__(self, user_settings=None, defaults=None):
+        if user_settings:
+            self._user_settings = self.__check_user_settings(user_settings)
+        self.defaults = defaults or DEFAULTS
+        self._cached_attrs = set()
+
+    @property
+    def user_settings(self):
+        if not hasattr(self, '_user_settings'):
+            self._user_settings = DEFAULTS
+        return self._user_settings
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError("Invalid API setting: '%s'" % attr)
+        try:
+            # Check if present in user settings
+            val = self.user_settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            raise ValueError(f'Invalid setting variable: {attr}')
+        # Cache the result
+        self._cached_attrs.add(attr)
+        setattr(self, attr, val)
+        return val
+
+    def reload(self):
+        for attr in self._cached_attrs:
+            delattr(self, attr)
+        self._cached_attrs.clear()
+        if hasattr(self, '_user_settings'):
+            delattr(self, '_user_settings')
+
+
+api_settings = APISettings(None, DEFAULTS)
+
+
+def reload_api_settings(*args, **kwargs):
+    setting = kwargs['setting']
+    if setting.lower() == 'common':
+        api_settings.reload()
+
+
+setting_changed.connect(reload_api_settings)
---
+++ setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+PARENT_DIR = Path(__file__).resolve(strict=True).parent
+
+
+def get_version():
+    """Generate new version for project"""
+    with open(PARENT_DIR / 'version') as f:
+        version = f.readline()
+        return version
+
+
+setup(
+    name="django-graphql",
+    version=get_version(),
+    description="A package that makes easier to use graphql with django.",
+    long_description="file: README.md",
+    url="https://shitalluitel.com.np",
+    author="Shital Luitel",
+    author_email="ctalluitel@gmail.com",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers"
+        "Operating System :: OS Independent"
+        "Programming Language :: Python"
+        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3 :: Only"
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        'django',
+        'djangorestframework'
+    ],
+)
