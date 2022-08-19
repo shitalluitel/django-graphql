a601b55946853b64bdd570db361dc22d2b9afe1c
Shital Babu Luitel <ctalluitel@gmail.com>
2022-08-19 13:37:55 +0545

    Refactor url for proper implementation

---
+++ djangographql/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+from .schema import schema
+
+urlpatterns = [
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+]
--- graphql/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-
-from graphql import schema
-from graphql.views import CustomGraphQLView
-
-urlpatterns = [
-    path('', csrf_exempt(CustomGraphQLView.as_view(graphql=True, schema=schema))),
-]
