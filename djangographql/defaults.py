acf9e6a6a7bc1a93657030d9faaab1ccabed1f30
Shital Babu Luitel <ctalluitel@gmail.com>
2022-08-19 13:33:50 +0545

    Added default settings for django-graphql

---
+++ djangographql/defaults.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class Defaults:
+    GRAPHQL_APPS: list = None
+    GRAPHENE = {
+        'SCHEMA': 'schema.schema',
+        'RELAY_CONNECTION_MAX_LIMIT': 10000,
+    }
+
+
+def update_defaults(default, class_name):
+    _class = globals()[class_name]
+    defaults = asdict(_class())
+    default.update(defaults)
+
+
+def get_defaults():
+    agg_defaults = {}
+    update_defaults(agg_defaults, 'Defaults')
+    return agg_defaults
