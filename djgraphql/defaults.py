1bb585d34d373ce615eb6c0d1e4f7833ecf4666b
Shital Babu Luitel <ctalluitel@gmail.com>
2022-08-22 15:44:10 +0545

    Change way of implementing default settings for package

--- djgraphql/defaults.py
+++ djgraphql/defaults.py
@@ -1,23 +1,6 @@
-from dataclasses import dataclass, asdict
-
-
-@dataclass
 class Defaults:
-    GRAPHQL_APPS: list = None
+    GRAPHQL_APPS = None
     GRAPHENE = {
         'SCHEMA': 'schema.schema',
-        'RELAY_CONNECTION_MAX_LIMIT': 10000,
+        'RELAY_CONNECTION_MAX_LIMIT': 10000
     }
-
-
-def update_defaults(default, class_name):
-    _class = globals()[class_name]
-    defaults = asdict(_class())
-    default.update(defaults)
-
-
-def get_defaults(agg_defaults=None):
-    if not agg_defaults:
-        agg_defaults = {}
-    update_defaults(agg_defaults, 'Defaults')
-    return agg_defaults
--- djgraphql/settings.py
+++ djgraphql/settings.py
@@ -1,50 +1,23 @@
 from django.conf import settings
 from django.test.signals import setting_changed
-from django.utils.module_loading import import_string
 
-from .defaults import get_defaults
-
-
-def perform_import(val, setting_name):
-    """
-    If the given setting is a string import notation,
-    then perform the necessary import or imports.
-    """
-    if val is None:
-        return None
-    elif isinstance(val, str):
-        return import_from_string(val, setting_name)
-    elif isinstance(val, (list, tuple)):
-        return [import_from_string(item, setting_name) for item in val]
-    return val
-
-
-def import_from_string(val, setting_name):
-    """
-    Attempt to import a class from a string representation.
-    """
-    try:
-        return import_string(val)
-    except ImportError as e:
-        msg = "Could not import '%s' for API setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
-        raise ImportError(msg)
+from djgraphql.defaults import Defaults
 
 
 class ImportSettings:
-    def defaults(self):
-        params = get_defaults()
+    @staticmethod
+    def defaults():
+        params = {key:value for key, value in Defaults.__dict__.items() if not key.startswith('__') and not callable(key)}
         new_defaults = {}
         for key, value in params.items():
             try:
                 new_defaults[key] = getattr(settings, key, value)
             except Exception as e:  # noqa
                 new_defaults[key] = value
-
         return new_defaults
 
 
-_settings = ImportSettings()
-DEFAULTS = _settings.defaults()
+DEFAULTS = ImportSettings.defaults()
 
 
 class APISettings:
